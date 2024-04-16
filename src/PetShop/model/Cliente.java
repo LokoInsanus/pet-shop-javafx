b9d944e44e616929f30229cedec523efb64a7464
Marcelo Pereira <53840695+LokoInsanus@users.noreply.github.com>
2024-04-16 09:46:32 -0300

    First Commit

---
+++ src/PetShop/model/Cliente.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PetShop.model;
+
+/**
+ *
+ * @author 20221si032@cefetes.br
+ */
+public class Cliente {
+    private int id;
+    private String nome;
+    private String email;
+    private String rua;
+    private String bairro;
+    private String numeroCasa;
+    private String numeroTelefone;
+    private String cpf;
+    
+    
+}
---
+++ src/PetShop/model/Pet.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PetShop.model;
+
+/**
+ *
+ * @author 20221si032@cefetes.br
+ */
+public class Pet {
+    private int id;
+    private String nome;
+    private String animal;
+    private Cliente dono;
+    private String raca;
+    private String rga;
+    
+    
+}
